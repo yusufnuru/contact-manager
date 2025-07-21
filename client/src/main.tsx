583cb5074cf1061d80db665f353ab93ccb4400de
yusufnuru <yusufnuru2002@gmail.com>
2025-07-21 18:16:12 +0800

    feat: wrap RouterProvider with AuthProvider for authentication context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import {createRouter, RouterProvider} from '@tanstack/react-router'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+import { AuthProvider } from './context/auth-context'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -24,7 +25,9 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
