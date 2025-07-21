d8afa7d7471167ab85622064dd8087d012034811
yusufnuru <yusufnuru2002@gmail.com>
2025-07-21 16:15:27 +0800

    Refactor database configuration to include models and improve connection handling

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,36 +1,39 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
-import pkg from 'pg'
-import { DB_URL } from '../constants/env'
+import { drizzle } from 'drizzle-orm/node-postgres';
+import pkg from 'pg';
+import { DB_URL } from '../constants/env.js';
+import * as ContactModel from '../models/contact-model.js';
+import * as ContactCategoryModel from '../models/contact-category-model.js';
 
-const { Pool } = pkg
+const { Pool } = pkg;
 const pool = new Pool({
   connectionString: DB_URL,
-})
+});
 
 export const db = drizzle(pool, {
-  schema: {},
-})
+  schema: {
+    ...ContactModel,
+    ...ContactCategoryModel,
+  },
+});
+
+export type PgDatabase = typeof db;
 
 export const connectToDatabase = async () => {
   try {
-    const client = await pool.connect()
-    console.log('✅ Successfully connected to PostgreSQL with Drizzle')
-    client.release()
+    const client = await pool.connect();
+    console.log('✅ Successfully connected to PostgreSQL with Drizzle');
+    client.release();
   } catch (error) {
-    console.error('❌ Failed to connect to PostgreSQL with Drizzle', error)
-    process.exit(1)
+    console.error('❌ Failed to connect to PostgreSQL with Drizzle', error);
+    process.exit(1);
   }
-}
+};
 
 export const closeDatabase = async () => {
   try {
-    await pool.end()
-    console.log('✅ Successfully Database Pool closed')
+    await pool.end();
+    console.log('✅ Successfully Database Pool closed');
   } catch (error) {
-    console.error('❌ Error closing database pool', error)
+    console.error('❌ Error closing database pool', error);
   }
-}
-
-export const cleanDb = () => {
-  return db.transaction(async (tx) => {})
-}
+};
